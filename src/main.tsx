a795f9654fad0614f52f609f2948d3d4e2ce6a94
José Ramón Vergara Ariztía <67248855+jramonv@users.noreply.github.com>
2025-08-30 17:19:22 -0600

    chore: refine service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { processQueue } from './lib/offlineQueue'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById('root')!
+createRoot(container).render(<App />)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+  })
+
+  window.addEventListener('online', () => {
+    processQueue()
+  })
+}
+
+if (navigator.onLine) {
+  processQueue()
+}
